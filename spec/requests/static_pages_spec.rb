2558934ab26e648bd0b9e63499a8d5890d0e4930
Tyler Wasden <twasden@gmail.com>
2013-09-20 08:31:40 -0700

    refactored rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+ror = "Ruby on Rails Tutorial Sample App"
 
 describe "Static pages" do
 
@@ -11,7 +12,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{ror} | Home")
     end
   end
 
@@ -24,7 +25,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{ror} | Help")
     end
   end
 
@@ -37,7 +38,7 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{ror} | About Us")
     end
   end
 
@@ -49,7 +50,7 @@ describe "Static pages" do
 
     it "should have the title 'Contact Us'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
+      expect(page).to have_title("#{ror} | Contact Us")
     end
   end
 
